a31568f832c8764e955ada6c0670dc39b841e9c6
achiu <mr.arthur.chiu@gmail.com>
2010-04-20 17:29:57 -0700

    updated test strap

--- test/teststrap.rb
+++ test/teststrap.rb
@@ -1,4 +1,14 @@
 require 'rubygems'
+require 'rr'
 require 'riot'
+require 'riot/rr'
+require 'rack/test'
 require 'rack/mock'
+require 'rack/utils'
+require 'rack/session/cookie'
+require 'rack/builder'
 require File.expand_path(File.join(File.dirname(__FILE__),'..','lib','rack-recaptcha'))
+
+class Rack::Situation
+  include Rack::Test::Methods
+end
