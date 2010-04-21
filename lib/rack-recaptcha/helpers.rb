cbe460e2a2047498b6a4c0ecd1d4d3a4f9ca5768
achiu <mr.arthur.chiu@gmail.com>
2010-04-20 17:30:05 -0700

    helpers test

---
+++ lib/rack-recaptcha/helpers.rb
@@ -0,0 +1,54 @@
+module Rack
+  class Recaptcha
+    module Helpers
+
+      DEFAULT= {
+        :public_key => ( Rack::Recaptcha.public_key || ENV['RACK_RECAPTCHA_PUBLIC_KEY']),
+        :height => 300,
+        :width => 500,
+        :row => 3,
+        :cols => 5
+      }
+
+
+      def recaptcha_tag(type= :noscript, options={})
+        options.reverse_merge! DEFAULT
+        html = case type.to_sym
+        when :challenge
+          (<<-CHALLENGE).gsub(/^ #{10}/,'')
+          <script type="text/javascript" src="http://api.recaptcha.net/challenge?k=#{options[:public_key]}>">
+          </script>
+          CHALLENGE
+        when :noscript
+          (<<-NOSCRIPT).gsub(/^ #{10}/,'')
+          <noscript>
+          <iframe src="http://api.recaptcha.net/noscript?k=#{options[:public_key]}" height="#{options[:height]}" width="#{options[:width]}" frameborder="0"></iframe><br>
+          <textarea name="recaptcha_challenge_field" rows="#{options[:row]}" cols="#{options[:cols]}"></textarea>
+          <input type="hidden" name="recaptcha_response_field" value="manual_challenge">
+          </noscript>
+          NOSCRIPT
+        when :ajax
+          (<<-AJAX).gsub(/^ #{10}/,'')
+          <div id="ajax_recaptcha"></div>
+          <script type="text/javascript" src="http://api.recaptcha.net/js/recaptcha_ajax.js"></script>
+          <script type="text/javascript">
+          Recaptcha.create('#{options[:key]}', document.getElementById('ajax_recaptcha')#{options[:display] ? ',RecaptchaOptions' : ''});
+          </script>
+          AJAX
+        else
+          ""
+        end
+        if options[:display]
+          (<<-DISPLAY).gsub(/^ #{10}/,'')
+          <script type="text/javascript">
+          var RecaptchaOptions = #{options[:display].to_json};
+          </script>
+          DISPLAY
+        else
+          ""
+        end + html
+      end
+
+    end
+  end
+end
---
+++ test/helpers_test.rb
@@ -0,0 +1,50 @@
+require File.expand_path(File.join(File.dirname(__FILE__),'teststrap'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','lib','rack-recaptcha','helpers'))
+
+class Helper
+  include Rack::Recaptcha::Helpers
+end
+
+context "Rack::Recaptcha::Helpers" do
+
+  context "recaptcha_tag" do
+    setup do
+      @helper = Helper.new
+    end
+
+    context "ajax" do
+      context "with display" do
+        setup { @helper.recaptcha_tag(:ajax,:display => {:theme => 'red'}) }
+        asserts("has js") { topic }.matches %r{recaptcha_ajax.js}
+        asserts("has div") { topic }.matches %r{<div id="ajax_recaptcha"></div>}
+        asserts("has display") { topic }.matches %r{RecaptchaOptions}
+        asserts("has red theme") { topic }.matches %r{"theme":"red"}
+      end
+      context "without display" do
+        setup { @helper.recaptcha_tag(:ajax) }
+        asserts("has js") { topic }.matches %r{recaptcha_ajax.js}
+        asserts("has div") { topic }.matches %r{<div id="ajax_recaptcha"></div>}
+        asserts("has display") { topic =~ %r{RecaptchaOptions} }.not!
+        asserts("has red theme") { topic =~ %r{"theme":"red"} }.not!
+      end
+    end
+
+    context "noscript" do
+      setup { @helper.recaptcha_tag :noscript, :public_key => "hello_world_world" }
+      asserts("iframe") { topic }.matches %r{iframe}
+      asserts("no script tag") { topic }.matches %r{<noscript>}
+      asserts("public key") { topic }.matches %r{hello_world_world}
+      asserts("has js") { topic =~ %r{recaptcha_ajax.js} }.not!
+    end
+
+    context "challenge" do
+      setup { @helper.recaptcha_tag(:challenge) }
+      asserts("has script tag") { topic }.matches %r{script}
+      asserts("has challenge js") { topic }.matches %r{challenge}
+      asserts("has js") { topic =~ %r{recaptcha_ajax.js} }.not!
+      asserts("has display") { topic =~ %r{RecaptchaOptions} }.not!
+    end
+  end
+
+
+end
