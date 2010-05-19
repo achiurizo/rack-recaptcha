3cdd24fa3eca455b91e8ab4d779fce1f68559c6e
minad <dmendler@wurzelteiler.de>
2010-05-19 17:52:31 +0800

    cleanup helper

--- lib/rack/recaptcha/helpers.rb
+++ lib/rack/recaptcha/helpers.rb
@@ -9,44 +9,35 @@ module Rack
         :cols => 5
       }
 
-
       def recaptcha_tag(type= :noscript, options={})
         options = DEFAULT.merge(options)
         options[:public_key] ||= Rack::Recaptcha.public_key
         path = options[:ssl] ? Rack::Recaptcha::API_SECURE_URL : Rack::Recaptcha::API_URL
         html = case type.to_sym
         when :challenge
-          (<<-CHALLENGE).gsub(/^ #{10}/,'')
-          <script type="text/javascript" src="#{path}/challenge?k=#{options[:public_key]}">
-          </script>
-          CHALLENGE
+          %{<script type="text/javascript" src="#{path}/challenge?k=#{options[:public_key]}">
+            </script>}.gsub(/^ +/, '')
         when :noscript
-          (<<-NOSCRIPT).gsub(/^ #{10}/,'')
-          <noscript>
-          <iframe src="#{path}/noscript?k=#{options[:public_key]}" height="#{options[:height]}" width="#{options[:width]}" frameborder="0"></iframe><br>
-          <textarea name="recaptcha_challenge_field" rows="#{options[:row]}" cols="#{options[:cols]}"></textarea>
-          <input type="hidden" name="recaptcha_response_field" value="manual_challenge">
-          </noscript>
-          NOSCRIPT
+          %{<noscript>
+            <iframe src="#{path}/noscript?k=#{options[:public_key]}" height="#{options[:height]}" width="#{options[:width]}" frameborder="0"></iframe><br>
+            <textarea name="recaptcha_challenge_field" rows="#{options[:row]}" cols="#{options[:cols]}"></textarea>
+            <input type="hidden" name="recaptcha_response_field" value="manual_challenge">
+            </noscript>}.gsub(/^ +/, '')
         when :ajax
-          (<<-AJAX).gsub(/^ #{10}/,'')
-          <div id="ajax_recaptcha"></div>
-          <script type="text/javascript" src="#{path}/js/recaptcha_ajax.js"></script>
-          <script type="text/javascript">
-          Recaptcha.create('#{options[:public_key]}', document.getElementById('ajax_recaptcha')#{options[:display] ? ',RecaptchaOptions' : ''});
-          </script>
-          AJAX
+          %{<div id="ajax_recaptcha"></div>
+            <script type="text/javascript" src="#{path}/js/recaptcha_ajax.js"></script>
+            <script type="text/javascript">
+            Recaptcha.create('#{options[:public_key]}', document.getElementById('ajax_recaptcha')#{options[:display] ? ',RecaptchaOptions' : ''});
+            </script>}.gsub(/^ +/, '')
         else
-          ""
+          ''
         end
         if options[:display]
-          (<<-DISPLAY).gsub(/^ #{10}/,'')
-          <script type="text/javascript">
-          var RecaptchaOptions = #{options[:display].to_json};
-          </script>
-          DISPLAY
+          %{<script type="text/javascript">
+            var RecaptchaOptions = #{options[:display].to_json};
+            </script>}.gsub(/^ +/, '')
         else
-          ""
+          ''
         end + html
       end
 
